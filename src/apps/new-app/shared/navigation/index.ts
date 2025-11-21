d10678dad7903e0b80056f88fa0a10031ad20a65
mzhvv <maximmezhevov@gmail.com>
2025-11-21 08:29:55 +0300

    .

--- src/apps/new-app/shared/navigation/index.ts
+++
@@ -1,5 +0,0 @@
-// src/apps/new-app/shared/navigation/index.ts
-
-import type { DomainNavigationKey } from '@apps/new-app/shared/types/rnc'
-
-export const domainNavigation = ['/new-app'] as const satisfies DomainNavigationKey[]
