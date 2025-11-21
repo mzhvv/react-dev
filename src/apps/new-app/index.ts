0f8c86abe0bcfa73029f0c118aa0fb411131da90
mzhvv <maximmezhevov@gmail.com>
2025-11-21 08:29:55 +0300

    .

--- src/apps/new-app/index.ts
+++
@@ -1,7 +0,0 @@
-// src/apps/new-app/index.ts
-/** @private - только для src/apps/__accumulators__/* */
-
-export type { DomainRelativePath as NewAppDomainRelativePath } from './shared/types/rnc'
-export { routes as newAppRoutes } from './app/routes'
-export { domainNavigation as newAppDomainNavigation } from './shared/navigation'
-export { useNavigationConstants as useNewAppNavigationConstants } from './shared/constants/use-navigation'
--- src/apps/new-app/page.tsx
+++
@@ -1,5 +0,0 @@
-// src/apps/new-app/page.tsx
-
-export const NewApp = () => {
-  return <div>✅ App new-app created successfully!</div>
-}
