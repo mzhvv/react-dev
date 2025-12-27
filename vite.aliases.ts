160b9dffbfe87596a89c9aa866567abb86aa944d
mzhvv <maximmezhevov@gmail.com>
2025-12-27 12:38:47 +0300

    .

--- vite.aliases.ts
+++ vite.aliases.ts
@@ -1,25 +1,25 @@
 // vite.aliases.ts
 
-import path from 'path'
+import { resolve } from 'path'
 import type { AliasOptions } from 'vite'
 
 export const alias = {
-  '@public': path.resolve(__dirname, './public'),
+  '@public': resolve(__dirname, './public'),
 
-  '@core/features': path.resolve(__dirname, './src/core/features'),
-  '@core/libs': path.resolve(__dirname, './src/core/shared/libs'),
-  '@core/utils': path.resolve(__dirname, './src/core/shared/utils'),
-  '@core/types': path.resolve(__dirname, './src/core/types'),
-  '@core': path.resolve(__dirname, './src/core'),
+  '@core/features': resolve(__dirname, './src/core/features'),
+  '@core/libs': resolve(__dirname, './src/core/shared/libs'),
+  '@core/utils': resolve(__dirname, './src/core/shared/utils'),
+  '@core/types': resolve(__dirname, './src/core/types'),
+  '@core': resolve(__dirname, './src/core'),
 
-  '@react-dev/aggregator': path.resolve(__dirname, './src/react-dev/__aggregator__'),
-  '@react-dev': path.resolve(__dirname, './src/react-dev'),
+  '@react-dev/aggregator': resolve(__dirname, './src/react-dev/__aggregator__'),
+  '@react-dev': resolve(__dirname, './src/react-dev'),
 
-  '@template-app': path.resolve(__dirname, './src/apps/__template-app__'),
-  '@apps/accumulators': path.resolve(__dirname, './src/apps/__accumulators__'),
-  '@apps': path.resolve(__dirname, './src/apps'),
+  '@template-app': resolve(__dirname, './src/apps/__template-app__'),
+  '@apps/accumulators': resolve(__dirname, './src/apps/__accumulators__'),
+  '@apps': resolve(__dirname, './src/apps'),
 
-  '@ui': path.resolve(__dirname, './src/ui'),
+  '@ui': resolve(__dirname, './src/ui'),
 
-  '@': path.resolve(__dirname, './src'),
+  '@': resolve(__dirname, './src'),
 } as const satisfies AliasOptions
