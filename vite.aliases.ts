1fa8357494906173c62f3773756529182de27370
mzhvv <maximmezhevov@gmail.com>
2026-05-03 14:49:54 +0300

    .

--- vite.aliases.ts
+++ vite.aliases.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 import type { AliasOptions } from 'vite'
 
 export const alias = {
-  // #region apps
+  // #region apps(microfrontends)
 
   '@home': resolve(__dirname, './src/apps/home'),
   '@documentation': resolve(__dirname, './src/apps/documentation'),
@@ -19,10 +19,12 @@ export const alias = {
   '@packages': resolve(__dirname, './src/packages'),
   '@core': resolve(__dirname, './src/core'),
   '@business': resolve(__dirname, './src/business'),
+
   '@accumulator': resolve(__dirname, './src/apps/__accumulator__'),
   '@apps': resolve(__dirname, './src/apps'),
   '@aggregator': resolve(__dirname, './src/main/__aggregator__'),
   '@main': resolve(__dirname, './src/main'),
+
   '@': resolve(__dirname, './src'),
   '@public': resolve(__dirname, './public'),
 
